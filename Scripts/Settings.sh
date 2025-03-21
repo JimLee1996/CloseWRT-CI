666c6a992735db21ac9dd72a315fbca772674ec6
JimLee1996 <59061463@qq.com>
2025-03-21 14:11:30 +0800

    feat: n60-pro

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 #修改immortalwrt.lan关联IP
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 #添加编译日期标识
@@ -21,21 +19,7 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
-#配置文件修改
-echo "CONFIG_PACKAGE_luci=y" >> ./.config
-echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
-echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
-echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
-
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo -e "$WRT_PACKAGE" >> ./.config
 fi
-
-#调整mtk系列配置
-sed -i '/TARGET.*mediatek/d' ./.config
-sed -i '/TARGET_MULTI_PROFILE/d' ./.config
-sed -i '/TARGET_PER_DEVICE_ROOTFS/d' ./.config
-cat $GITHUB_WORKSPACE/Config/$WRT_CONFIG.txt >> .config
